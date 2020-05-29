8765a4c124bd6baab4e84a9b22271a34d16b1572
Paula Giovanna Rodrigues <58047035+paulagiovanna@users.noreply.github.com>
2020-05-29 19:42:30 -0300

    Update random_walk.py
    random walk with grid search

--- random_walk.py
+++ random_walk.py
@@ -2,7 +2,9 @@ import pandas as pd
 import numpy as np
 import networkx as nx
 import random
+from scipy.stats import pearsonr
 
+'''Carrega o arquivo de voos'''
 international_flights = pd.read_csv('international_graph.csv')
 international_flights = international_flights[['pais_origem', 'pais_destino', 'qtde_voos']]
 column_list = international_flights.columns.values.tolist() 
@@ -10,6 +12,7 @@ column_list = international_flights.columns.values.tolist()
 source = international_flights['pais_origem'].unique()
 target = international_flights['pais_destino'].unique()
 
+'''Coloca na base os registros de paises que nao tem voos de chegada ou de partida'''
 for country in source:
     if country not in target:  
         #print('pais_origem: '+str(country))
@@ -23,6 +26,7 @@ for country in target:
         international_flights = international_flights.append(df, ignore_index = True) 
 
 
+'''Faz o grafo de voos entre paises'''
 #print(international_flights)
 country_international_flights = international_flights.groupby(['pais_origem']).sum() 
 transition_matrix = international_flights.copy()
@@ -31,6 +35,7 @@ transition_matrix['prob'] = 0.
 #print(country_international_flights)
 #print(transition_matrix)
 
+'''Poe nas arestas a probabilidade de um voo ocorrer do pais a para o pais b'''
 for index, row in international_flights.iterrows():
     weight = country_international_flights.loc[row['pais_origem']]
     weight = float(weight)
@@ -41,34 +46,76 @@ for index, row in international_flights.iterrows():
 transition_matrix = transition_matrix.set_index('pais_origem')  
 transition_matrix.to_csv("transition_matrix_edge_list.csv")
 
+'''Transforma o dataframe em uma matriz de correlacao'''
 transition_matrix = transition_matrix.pivot(columns='pais_destino', values='prob') 
 transition_matrix = transition_matrix.fillna(0)
 #print(transition_matrix)                  
 
-#print(transition_matrix.iloc[[25]])
-state_vector = [0]*142
-state_vector[25] = 1000
-#print(state_vector) 
+#print(transition_matrix.iloc[[24]])
 
-country_list = transition_matrix.index.values.tolist()
+'''Carrega o arquivo com o total de casos diarios'''
+total_cases = pd.read_csv('total_cases_countries_normalized.csv')
+total_cases = total_cases[['Name', 'Day', 'DailyCases']].set_index(['Name', 'Day'])
 
-for i in range(30):
-    state_vector = np.dot(state_vector, transition_matrix)
-    #print(state_vector)
+countries = transition_matrix.columns.to_list()
 
-i = 0
-df = pd.DataFrame()
-df['country'] = ' '
-df['state'] = 0
+indexes = []
+for index in total_cases.index:
+    indexes.append(index)
 
-for country in country_list:
-    df = df.append({'country': country, 'state': state_vector[i]}, ignore_index = True)
-    i = i+1
+daily_cases = []
+for country in countries:
+    if (country, 77) not in indexes:
+        daily_cases.append(0.) 
+    else:
+        daily_cases.append(total_cases.loc[country, 77][0])
 
-df = df.set_index(['country'])
-df.sort_values(by='state', ascending=False, inplace=True)
+#daily_cases = np.reshape(daily_cases,(-1, 1)) 
 
-df.to_csv('state_vector.csv')
-transition_matrix.to_csv('transition_matrix.csv')
-#print(df)
+df_final = pd.DataFrame()
+df_final['daily_cases'] = daily_cases
 
+arquivo = open("resultados.csv", "w")
+arquivo.write('v;r\n')
+
+respostas = open("respostas.csv","w")
+
+contamination_rate = [float(x)/10.0 for x in range(10,31,1)]
+initial_number = [x for x in range(10,510,10)] 
+
+def pearsonr_pval(x,y):
+        return pearsonr(x,y)[1]
+
+for v in initial_number:
+    for r in contamination_rate:
+        state_vector = [0]*129
+        state_vector[24]=v  
+        for i in range(78):
+            if i == 0:
+                state_vector = np.dot(state_vector, transition_matrix)
+            else:
+                state_vector = np.dot(state_vector, transition_matrix)
+                state_vector_multiply = [state_country*r for state_country in state_vector]
+                state_vector = state_vector_multiply
+        df_final['state_vector'] = state_vector
+        
+
+        df_corr = df_final.corr(method='pearson', min_periods = 6)
+        df_p = df_final.corr(method=pearsonr_pval, min_periods = 6)
+        df_corr_display = df_corr.round(2).applymap(str) + ' p=' + df_p.round(2).applymap(str) + ''
+        '''Pearson na mao
+        cov = np.cov(state_vector,daily_cases)
+        prod_var = np.var(state_vector) * np.var(daily_cases)
+        sqrt_prod_var = np.sqrt(prod_var)
+        pearson = cov[0,1]/sqrt_prod_var
+        '''
+        '''Funcao pearsonr 
+        state_vector = np.reshape(state_vector,(-1,1))
+        daily_cases = np.reshape(daily_cases,(-1, 1))
+        pearson = pearsonr(daily_cases,state_vector)[1]
+        print('pearson: ' + str(pearson))
+        '''
+        arquivo.write(str(v)+';'+str(r)+';'+'\n'+str(df_corr_display)+';'+'\n\n')
+        respostas.write("v:;"+str(v)+";r:;"+str(r)+"\n"+str(df_final)+"\n\n")
+arquivo.close()
+respostas.close()
